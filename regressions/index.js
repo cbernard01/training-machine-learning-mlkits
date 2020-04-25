8da98e6b79f6bcef5e1dae056432101c71296d00
Clifford Bernard <clifford@verticalfront.com>
2020-04-25 06:20:36 -0500

    Gradient Descent TensorFlow Commit:
    Updated the regression algorithm with a TensorFlow form of the Gradient Descent calculations.  Created features for batch sizing and multiple features.

--- regressions/index.js
+++ regressions/index.js
@@ -7,14 +7,16 @@ const LinearRegression = require("./linear-regression");
 let {features, labels, testFeatures, testLabels} = loadCSV("cars.csv", {
   shuffle: true,
   splitTest: 50,
-  dataColumns: ["horsepower"],
+  dataColumns: ["horsepower", "weight", "displacement"],
   labelColumns: ["mpg"]
 });
 
-const regression = new LinearRegression(features, labels, {
-  learningRate: 0.001,
-  iterations: 100
+const regression = new LinearRegression(features, labels, testFeatures, testLabels, {
+  learningRate: 0.1,
+  iterations: 3,
+  batchSize: 10
 });
 
 regression.train();
-
+regression.test();
+regression.predict([["120", "2","380"]]).print();
